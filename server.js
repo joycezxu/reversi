1096abd1bc20b895f11102b1d0b93be601e2c676
Joyce Xu <joyce.z.xu@gmail.com>
2018-05-13 14:12:43 -0700

    v2

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ var file = new static.Server(directory);
 
 /* Construct an http server that gets files from the file server */ 
 var app = http.createServer(
-		function(request, response){
+		function(request,response){
 			request.addListener('end',
 				function() {
-					file.serve(reqeust, response);
+					file.serve(request, response);
 				}
 			).resume ();
 		}
